fa4b7ab882a25f590ec77a7e9d8d217245890ddc
Fatih Yolal <fatihyolal@yahoo.com>
2023-08-25 17:59:42 +0300

    image-upload

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,6 +14,7 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
+    
     if @list.save
       redirect_to list_path(@list)
     else
@@ -22,6 +23,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
